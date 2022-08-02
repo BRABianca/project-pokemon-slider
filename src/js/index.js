cadc7b88a969a5a880e81d48f4dbbe3a85aa2bce
Bianca Ávalos <108427943+BRABianca@users.noreply.github.com>
2022-08-02 16:21:51 -0400

    My first commit
    Simple but efficient, a way to start my career in programming. 
    I will soon commit another one in the same tamplate. 
    Hope you enjoy.

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+const cartoes = document.querySelectorAll('.cartao');
+let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao){
+       cartoes[indiceCartao].classList.add('selecionado');
+}
+
+btnAvancar.addEventListener('click', function (){
+    esconderCartaoSelecionado();
+    
+    if(cartaoAtual === cartoes.length - 1) {
+        const cartaoSelecionado = document.querySelector('.fundo-1');
+        cartaoSelecionado.classList.add('selecionado');
+        cartaoAtual = 0;
+    } else {
+            cartaoAtual++;
+    }
+
+    mostrarCartao(cartaoAtual);
+
+});
+
+btnVoltar.addEventListener('click', function (){
+    esconderCartaoSelecionado();
+
+    if (cartaoAtual === 0) {
+    	const cartaoSelecionado = document.querySelector('.fundo-3');
+    	cartaoSelecionado.classList.add('selecionado');
+    	cartaoAtual = cartoes.length - 1;
+    } else {
+    	cartaoAtual--;
+    }
+
+    mostrarCartao(cartaoAtual);
+
+});
